969edde2f87b68fa0e80adafe6adf1d5b33085c9
Megumi-cpu <20241391@lamolina.edu.pe>
2025-12-28 01:47:17 -0500

    creacion inicial de scraper e importacion de librerias

---
+++ scraper.py
@@ -0,0 +1,8 @@
+# Módulo de Scraping para INFO-CANASTA
+# Encargado de: Extraer precios de PlazaVea
+import requests
+from bs4 import BeautifulSoup
+import random
+
+# TODO: Implementar lógica de user-agents
+# TODO: Implementar función de extracción
